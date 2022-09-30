02abe26ff57cdc105c20a8949421dcf16dec1f1c
Abdelhamid salhi <essalhi12345@gmail.com>
2022-09-30 19:51:42 +0100

    Deleted unecessary dependencies and added the java faker dependency and tested it.

--- src/main/java/com/entrepriseName/ApiAppApplication.java
+++ src/main/java/com/entrepriseName/ApiAppApplication.java
@@ -1,5 +1,6 @@
 package com.entrepriseName;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiAppApplication.class, args);
-	}
 
+		SpringApplication.run(ApiAppApplication.class, args);
+
+		Faker faker = new Faker();
+
+		String name = faker.name().fullName();
+		String firstName = faker.name().firstName();
+		String lastName = faker.name().lastName();
+		String streetAddress = faker.address().streetAddress();
+
+		System.out.println("fullName: "  + name+ " |firstName: " + firstName + " |lastName: " + lastName +
+				" |streetAddress: " + streetAddress);
+	}
 }
