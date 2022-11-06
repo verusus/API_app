736e9a64569d5bd6b97416085f1c64663fc922b7
Abdelhamid salhi <essalhi12345@gmail.com>
2022-11-06 14:30:55 +0100

    I added a unit test on real names.

--- src/test/java/com/entrepriseName/service/UserServiceTest.java
+++ src/test/java/com/entrepriseName/service/UserServiceTest.java
@@ -100,9 +100,12 @@ public class UserServiceTest {
 
     @Test
     public void getUserTestRealNames_ok() throws Exception {
-        Pattern patten = Pattern.compile("[a-zA-Z0-9]{8,}");
-        userService.getUsers(10).forEach(user -> Assertions.assertTrue(patten.matcher(user.getPassword()).find()));
+        Pattern patten = Pattern.compile("[a-zA-Z]{2,20}"); // no numbers || special characters are allowed
+
+        userService.getUsers(10).forEach(user -> Assertions.assertTrue(patten.matcher(user.getFirstName()).find()));
+        userService.getUsers(10).forEach(user -> Assertions.assertTrue(patten.matcher(user.getLastName()).find()));
     }
+
 
 
 
